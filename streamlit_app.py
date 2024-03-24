18a500059f8f832ee2667637c8262a5e9a5a486f
martingeew <67925955+martingeew@users.noreply.github.com>
2024-03-24 18:42:09 +0000

    Update df load

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,16 @@ import pandas as pd
 def create_label(row):
     return f"{row['Direction']}, {row['Citizenship']}"
 
+def load_data():
+    df = pd.read_pickle("df_citizenship_direction_202312.pkl")
+    return df
+
 
 # Main title for the dashboard
 st.title("New Zealand Migration Trends")
 
 @st.cache_data  # Use Streamlit's cache to load the data only once
-df = pd.read_pickle("df_citizenship_direction_202312.pkl")
+df = load_data()
 
 directions = st.multiselect(
             "Select directions:",

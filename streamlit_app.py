086b015fac64fbb0107dd94d74b285e3f778658c
martingeew <67925955+martingeew@users.noreply.github.com>
2024-03-24 18:34:36 +0000

    add data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 # Main title for the dashboard
 st.title("New Zealand Migration Trends")
+
+@st.cache_data  # Use Streamlit's cache to load the data only once
+df = pd.read_pickle("df_citizenship_direction_202312.pkl")
